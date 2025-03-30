70294faa8c89b2fe8cf16d3645eaf58d7928c14d
adity <adity.sahibgnaj@gmail.com>
2025-03-31 01:30:04 +0530

    My Repo

--- models/category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const categorySchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true } // Ensure 'name' is required
-});
-
-const Category = mongoose.model('Category', categorySchema);
-module.exports = Category;
